44c016b2ff2daa54981e3a45db0f3f2763092805
hc0405 <hc0405@gmail.com>
2024-01-03 12:32:44 +0800

    debug log again

--- api/server/utils/streamResponse.js
+++ api/server/utils/streamResponse.js
@@ -23,7 +23,13 @@ const sendMessage = (res, message, event = 'message') => {
     return;
   }
   let ok = res.write(`event: ${event}\ndata: ${JSON.stringify(message)}\n\n`);
-  logger.warn(`${JSON.stringify(message)} res:${ok}`);
+  //logger.warn(`${JSON.stringify(message)} res:${res}`);
+  //logger.warn(`Socket closed:${res.closed} status:${res.statusCode} finished:${res.finished}`)
+  if (!ok) {
+    logger.warn(
+      `Socket closed:${res.closed} status:${res.statusCode} finished:${res.finished} writable:${res.writable} writableLength:${res.writableLength}  destroyed:${res.destroyed} writableFinished:${res.writableFinished}`,
+    );
+  }
   //logger.warn(ok);
 };
 
